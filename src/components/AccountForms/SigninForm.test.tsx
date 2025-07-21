8f1afbdd06ede60c7cac73d63eb64996bf8cfe56
Nam Le <namphgle@gmail.com>
2025-07-20 20:51:16 -0700

    signin form path update

--- src/components/AccountForms/SigninForm.test.tsx
+++ src/components/AccountForms/SigninForm.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import SignInForm from './SignInForm';
+import SignInForm from './SigninForm';
 import { vi } from 'vitest';
 
 describe('SignInForm', () => {
