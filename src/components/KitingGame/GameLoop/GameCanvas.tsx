36011db35dd2bd5655e73a31083648ee5d39de25
Nam Le <namphgle@gmail.com>
2025-07-23 19:34:32 -0700

    path for assest update=

--- src/components/KitingGame/GameLoop/GameCanvas.tsx
+++ src/components/KitingGame/GameLoop/GameCanvas.tsx
@@ -5,9 +5,9 @@ import { distance } from '../Utils/collisionUtils';
 import type { Position } from '../Types/Position';
 import { drawPlayerSprite } from '../Utils/drawPlayerSprite';
 import { drawMushroomSprite } from '../Utils/drawMushroomSprite';
-import mushroomSprite from '/src/components/KitingGame/assets/mushroom_sprite.png'
-import playerSprite from '../assets/player_sprite.png';
-import bgScroll from '../assets/bg_scroll.jpg';
+import mushroomSprite from './assets/mushroom_sprite.png'
+import playerSprite from './assets/player_sprite.png';
+import bgScroll from './assets/bg_scroll.jpg';
 import { drawPlayerStats } from '../Utils/drawPlayerStats';
 import type { ItemData } from '../../../constants/itemData';
 
